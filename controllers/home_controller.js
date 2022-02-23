9066a0be421a8684c1f5b56e475e781d35ad30fd
mrpiyushprajapati <mrpiyushprajapati@gmail.com>
2022-02-23 17:20:16 +0530

    added post functionality

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,24 @@
+const Post = require('../models/post');
+
 module.exports.home=function(req, res){
     // return res.end('<h1>express is up for connect</h1>');
 
-    console.log(req.cookies);
-    return res.render('home', {
-        title: 'Home'
-    });
+    // console.log(req.cookies);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: 'lets CONNECT',
+    //         posts: posts
+    //     });
+    // })
+
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: 'lets CONNECT',
+            posts: posts
+        });
+    })
 }
 
 //module.exports.actionName=function(req, res){}
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 const homeController=require('../controllers/home_controller');
 
-console.log('roter loaded');
+console.log('router loaded');
 
-router.get('/',homeController.home);
-router.use('/users',require('./users'));
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 //for adding more routes access from here
 //router.use('/routerName', require('./routerFile'));
