0b7868f4c699f37e63b6e95d4c17935ecdc99d23
mrpiyushprajapati <mrpiyushprajapati@gmail.com>
2022-02-27 14:29:03 +0530

    added delete post functionaltiy

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,7 +22,7 @@ module.exports.create = function(req, res){
                 post.comments.push(comment);
                 post.save();
 
-                res.redirect('/');
+                return res.redirect('/');
             });
         }
     });
